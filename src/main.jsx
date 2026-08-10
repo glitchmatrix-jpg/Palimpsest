8257e4584a2298afd1c385eed4fc9b7244acbb0b
glitchmatrix-jpg <extraextra335@gmail.com>
2026-08-11 01:26:18 +0500

    fix: load polished portal transition styles

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import './styles/tokens.css';
 import './styles/base.css';
 import './styles/home.css';
 import './styles/motion.css';
+import './styles/portal-transition.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
